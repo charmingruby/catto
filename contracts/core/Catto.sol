fcff579375e105629e8931eba8093f7b2ae86f2e
charmingruby <mrrubycanal3105@gmail.com>
2025-04-27 10:38:26 -0300

    feat: adds openzeppelin contract inheritance

--- contracts/core/Catto.sol
+++ contracts/core/Catto.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Catto {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Catto is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
+    constructor() ERC721("Catto", "CAT") {}
+
     function helloWorld() public pure returns (string memory) {
         return "Hello World";
     }
