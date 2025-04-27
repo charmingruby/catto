ff453581dfa158d3b8a35a2348050dc805f72d36
charmingruby <mrrubycanal3105@gmail.com>
2025-04-27 12:36:51 -0300

    feat: impls base methods of erc721 from oz

--- contracts/core/Catto.sol
+++ contracts/core/Catto.sol
@@ -4,17 +4,57 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Catto is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
+    uint private _tokenIdCounter;
+
     constructor() ERC721("Catto", "CAT") {}
 
-    function helloWorld() public pure returns (string memory) {
-        return "Hello World";
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return string.concat(super.tokenURI(tokenId), ".json");
+    }
+
+    function mint() public {
+        _tokenIdCounter++;
+        _safeMint(msg.sender, _tokenIdCounter);
+        _setTokenURI(_tokenIdCounter, Strings.toString(_tokenIdCounter));
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://catto.xyz/";
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
     }
 }
