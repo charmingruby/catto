bf8a743220ed13ea8338da0db5ad3fc307dde5d6
charmingruby <mrrubycanal3105@gmail.com>
2025-05-08 09:53:47 -0300

    feat: changes oz erc721 to azuki erc721a

--- contracts/core/Catto.sol
+++ contracts/core/Catto.sol
@@ -4,57 +4,43 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-
-contract Catto is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
+contract Catto is ERC721A {
     uint private _tokenIdCounter;
+    address payable private _owner;
 
-    constructor() ERC721("Catto", "CTT") {}
+    constructor() ERC721A("Catto", "CTT") {
+        _owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Insufficient permission");
+        _;
+    }
+
+    function burn(uint256 tokenId) external {
+        super._burn(tokenId);
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = _owner.call{value: amount}("");
+        require(success == true, "Failed to withdraw");
+    }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    ) public view override(ERC721A) returns (string memory) {
         return string.concat(super.tokenURI(tokenId), ".json");
     }
 
-    function mint() public {
-        _tokenIdCounter++;
-        _safeMint(msg.sender, _tokenIdCounter);
-        _setTokenURI(_tokenIdCounter, Strings.toString(_tokenIdCounter));
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
+    function mint(uint256 quantity) public payable {
+        require(msg.value >= 0.01 ether * quantity, "Insufficient payment");
+        _mint(msg.sender, quantity);
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://catto.xyz/";
-    }
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(
-        address account,
-        uint128 value
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
     }
 }
